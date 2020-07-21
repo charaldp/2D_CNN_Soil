93fd3b773b3d5c2ec1114df08aebba3c7d200970
charaldp <charaldp@gmail.com>
2020-07-21 22:20:23 +0300

    Seaborn Spectra Diagrams

--- src/savgol.py
+++ src/savgol.py
@@ -3,9 +3,29 @@ import numpy as np
 import scipy
 from scipy import signal
 import matplotlib.pyplot as plt
+import seaborn as sns
+import argparse as argp
 
+
+
+def parse_args():
+	parser = argp.ArgumentParser(description='2D Convolutional Neural Network')
+	parser.add_argument('-is','--inputSpectra', type=str, help='Input spectra to use', default="../dataset/sources/reflectances.csv")
+	parser.add_argument('-li','--readLines', type=int, help='Read lines for the input spectra file', nargs='+', default=[-1])
+	parser.add_argument('-us','--undersampling', type=float, help='Undersampling to apply at input spcetra',default=1.0)
+	parser.add_argument('-md','--mode', type=str, help='Undersampling to apply at input spcetra',default='extract_abs')
+	args = parser.parse_args()
+	return args
+
+args = parse_args()
+print(args)
 print('Reading CSV...')
-data = pnd.read_csv('./reflectances.csv', low_memory=False)
+if args.readLines != -1:
+	data = pnd.read_csv(args.inputSpectra, low_memory=False, skiprows=lambda x: x not in args.readLines)
+else:
+	data = pnd.read_csv(args.inputSpectra, low_memory=False)
+print(data)
+
 # data2 = pnd.read_csv('./Absorbances_SG1_reduced.csv', low_memory=False)
 # data = pnd.read_csv('./reflectances.csv', nrows=1000)
 # data2 = pnd.read_csv('./Absorbances_SG1_reduced.csv', nrows=10)
@@ -20,6 +40,42 @@ print('Converting to np list...')
 data_array = data_array.tolist()
 print('Extracting savgol filter values...')
 data = None
+if args.mode == 'extract_plot':
+	for i in range(len(data_array)):
+		temp = data_array[i].pop(0)
+	# if args.undersampling != 1:
+	indices = []
+	columns = []
+	i = 0
+	length = len(data_array[0])
+	print(length)
+	indices_orig = range(length)
+	while i < length:
+		indices.append(i)
+		columns.append(0.5*i+400)
+		i += args.undersampling
+	print(len(indices))
+
+	x_1 = []
+	for i in range(len(data_array)):
+		x_1.append(np.interp(indices, indices_orig, data_array[i]))
+	data_array = x_1
+	print(args.undersampling)
+	# columns = range(400, 1500, 0.5 * args.undersampling)
+	# for i in range(len(columns)):
+	# 	columns[i] -= 0.5*i
+		# columns[i] = str(columns[i])
+	# columns.insert(0, 'ID')
+	# index = pnd.date_range("1 1 2000")
+	dtfr = pnd.DataFrame(data_array, columns=columns)
+	ax = sns.lineplot(data=dtfr.transpose())
+	# for ind, label in enumerate(plot_.get_xticklabels()):
+	# 	if ind % 10 == 0:  # every 10th label is kept
+	# 		label.set_visible(True)
+	# 	else:
+	# 		label.set_visible(False)
+	plt.show()
+	exit()
 # for test_index in range(1,1000, 3):
 # 	data_array[test_index].pop(0)
 	# data2_array[test_index].pop(0)
