537f012e251d9910f90d2b0eb9fec97298819c5a
charaldp <charaldp@gmail.com>
2020-10-11 16:26:51 +0300

    Diagrams metrics with predictions

--- src/diagramExtractor.py
+++ src/diagramExtractor.py
@@ -7,6 +7,7 @@ import pandas as pnd
 import argparse as argp
 import seaborn as sns
 from datetime import datetime
+import modelCNN2dSpectr
 
 mpl.rcParams.update({'font.size': 6})
 
@@ -60,6 +61,39 @@ def fixbarplots(ax, data_set, prop, width, metric, metrics_symbols):
         patch.set_x(patch.get_x() + diff * .5)
         plt.ylabel(prop+' '+metrics_symbols[metric], fontsize=12)
 
+def extractPropertyErrors(data_test, data_val, data_train, j, folder, prop, args):
+    test_pred = []
+    test_actual = []
+    val_pred = []
+    val_actual = []
+    train_pred = []
+    train_actual = []
+    print(folder, prop)
+    if (prop == 'Silt') and j < max(len(data_test), len(data_val), len(data_train)) - 1 and (not 'Silt_test_preds' in data_test[j]) and (not 'Silt_val_preds' in data_val[j])and (not 'Silt_train_preds' in data_train[j]):
+        if 'Test' in args.useSet:
+            print(data_test[j]['Clay_test_preds'])
+            test_pred = 100 - data_test[j]['Clay_test_preds'] - data_test[j]['Sand_test_preds']
+            test_actual = 100 - data_test[j]['Clay_test_actuals'] - data_test[j]['Sand_test_actuals']
+            print(test_pred)
+        if 'Val' in args.useSet:
+            val_pred = 100 - data_val[j]['Clay_val_preds'] - data_val[j]['Sand_val_preds']
+            val_actual = 100 - data_val[j]['Clay_val_actuals'] - data_val[j]['Sand_val_actuals']
+        if 'Train' in args.useSet:
+            train_pred = 100 - data_train[j]['Clay_train_preds'] - data_train[j]['Sand_train_preds']
+            train_actual = 100 - data_train[j]['Clay_train_actuals'] - data_train[j]['Sand_train_actuals']
+    elif prop+'_test_preds' in data_test[j]:                    
+        if 'Test' in args.useSet:
+            test_pred = data_test[j][prop+'_test_preds']
+            test_actual = data_test[j][prop+'_test_actuals']
+        if 'Val' in args.useSet:
+            val_pred = data_val[j][prop+'_val_preds']
+            val_actual = data_val[j][prop+'_val_actuals']
+        if 'Train' in args.useSet:
+            train_pred = data_train[j][prop+'_train_preds']
+            train_actual = data_train[j][prop+'_train_actuals']
+    return test_pred, test_actual, val_pred, val_actual, train_pred, train_actual
+        
+
 metrics_symbols = {'determ': "Coefficient of Determination", 'rpiq': "RPIQ", 'rmse': "Root Mean Squared Error"}
 OUTPUT_PATH = '../output/diagrams'
 if not os.path.exists(OUTPUT_PATH):
@@ -73,6 +107,12 @@ print(args)
 print(args.modelNames)
 # exit()
 if args.mode == 'metrics':
+    if 'Test' in args.useSet:
+        data_test_instances = []
+    if 'Val' in args.useSet:
+        data_val_instances = []
+    if 'Train' in args.useSet:
+        data_train_instances = []
     if 'Test' in args.useSet:
         data_test = {}
         for metric in args.metrics:
@@ -96,6 +136,12 @@ if args.mode == 'metrics':
                 data_train[metric][prop] = {}
     props = []
     for j, folder in enumerate(args.folders):
+        if 'Test' in args.useSet:
+            data_test_instances.append(pnd.read_csv(folder+'/test_predictions.csv', index_col=False))
+        if 'Val' in args.useSet:
+            data_val_instances.append(pnd.read_csv(folder+'/val_predictions.csv', index_col=False))
+        if 'Train' in args.useSet:
+            data_train_instances.append(pnd.read_csv(folder+'/train_predictions.csv', index_col=False))
         data = pnd.read_csv(folder+'/metrics.csv', index_col=0)
         properties = data.columns
         # datrans = data.T
@@ -144,22 +190,22 @@ if args.mode == 'metrics':
     exit()
 
 if args.mode == 'boxplots':
-    if 'Test' in args.useSet:
-        data_test = []
-    if 'Val' in args.useSet:
-        data_val = []
-    if 'Train' in args.useSet:
-        data_train = []
+    # if 'Test' in args.useSet:
+    data_test = []
+    # if 'Val' in args.useSet:
+    data_val = []
+    # if 'Train' in args.useSet:
+    data_train = []
     props = []
     center_offset = 0.24 + len(args.useSet)
     for j, folder in enumerate(args.folders):
         print(folder+'/test_predictions.csv')
-        if 'Test' in args.useSet:
-            data_test.append(pnd.read_csv(folder+'/test_predictions.csv', index_col=False))
-        if 'Val' in args.useSet:
-            data_val.append(pnd.read_csv(folder+'/val_predictions.csv', index_col=False))
-        if 'Train' in args.useSet:
-            data_train.append(pnd.read_csv(folder+'/train_predictions.csv', index_col=False))
+        # if 'Test' in args.useSet:
+        data_test.append(pnd.read_csv(folder+'/test_predictions.csv', index_col=False))
+        # if 'Val' in args.useSet:
+        data_val.append(pnd.read_csv(folder+'/val_predictions.csv', index_col=False))
+        # if 'Train' in args.useSet:
+        data_train.append(pnd.read_csv(folder+'/train_predictions.csv', index_col=False))
         # Search for properties at each new folder
         cols = data_test[0].columns
         for col in cols:
@@ -167,7 +213,9 @@ if args.mode == 'boxplots':
                 prop = col[0: col.find('_')]
                 if not prop in props:
                     props.append(prop)
+    print(data_test, data_val, data_train)
     print(props)
+    props = args.properties
     for prop in props:
         data = []
         labels = []
@@ -178,21 +226,15 @@ if args.mode == 'boxplots':
             sets_char = sets_char+set_char
         ax.set_title(prop+' '+sets_char+' Absolute Error')
         for j, folder in enumerate(args.folders):
-            # fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(6, 6), sharey=True)
-            # axs[1, 2].bxp(stats, showfliers=False)
-            # axs[1, 2].set_title('showfliers=False', fontsize=fs)
-            if prop+'_test_preds' in data_test[j]:
-                if 'Test' in args.useSet:
-                    errors_test = np.absolute(data_test[j][prop+'_test_preds'] - data_test[j][prop+'_test_actuals'])
-                if 'Val' in args.useSet:
-                    errors_val = np.absolute(data_val[j][prop+'_val_preds'] - data_val[j][prop+'_val_actuals'])
-                if 'Train' in args.useSet:
-                    errors_train = np.absolute(data_train[j][prop+'_train_preds'] - data_train[j][prop+'_train_actuals'])
-                # labels.append('')
-                labels.append(args.modelNames[j])# +' '+prop
-                # labels.append('')
-                # labels.append(args.modelNames[j]+' '+prop+'_val')
-                # labels.append(args.modelNames[j]+' '+prop+'_train')
+            test_pred, test_actual, val_pred, val_actual, train_pred, train_actual = extractPropertyErrors(data_test, data_val, data_train, j, folder, prop, args)
+            if 'Test' in args.useSet:
+                print(test_pred, test_actual)
+                errors_test = np.absolute(test_pred - test_actual)
+            if 'Val' in args.useSet:
+                errors_val = np.absolute(val_pred - val_actual)
+            if 'Train' in args.useSet:
+                errors_train = np.absolute(train_pred - train_actual)
+            labels.append(args.modelNames[j])# +' '+prop
             # print('N_test_preds', data_test['N_test_preds'])
             # print('N_test_actuals', data_test['N_test_actuals'],)
             # print('errors', errors)
